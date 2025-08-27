1310eae87e86ce78b6c81872089e1857a5d8b990
Huanye Zhu <zhuzhenghuanye@gmail.com>
2025-08-27 17:31:55 +0200

    added autoscroll to the top on changing pages for projects

--- src/main.tsx
+++ src/main.tsx
@@ -5,9 +5,11 @@ import App from './App.tsx'
 import { BrowserRouter } from 'react-router-dom'
 import { ThemeProvider } from '@emotion/react'
 import theme from './utils/theme.ts'
+import ScrollToTop from "./utils/ScrollToTop.tsx"
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <BrowserRouter>
+    <ScrollToTop />
     <ThemeProvider theme={theme}>
     <App />
     </ThemeProvider>
